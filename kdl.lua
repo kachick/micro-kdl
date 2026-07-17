e5ea3961f5cf89234f4f50ed33d4d1baa30c0fdb
aroum <aroum24@gmail.com>
2026-07-17 11:31:24 +0300

    feat: improve KDL syntax highlighting and support comments

--- kdl.lua
+++ kdl.lua
@@ -4,3 +4,10 @@ local config = import("micro/config")
 
 config.RegisterCommonOption("kdl", "kdl", true)
 config.AddRuntimeFile("kdl", config.RTSyntax, "kdl.yaml")
+
+-- Set the comment character format when a KDL buffer is opened
+function onBufferOpen(buf)
+    if buf.Settings["filetype"] == "kdl" then
+        buf.Settings["comment.type"] = "// %s"
+    end
+end
